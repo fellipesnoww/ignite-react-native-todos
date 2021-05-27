ffb04b05511a04dc88e9fe0893ac1d17c89e0142
Fellipe <fellipe_emanoel@univem.edu.br>
2021-05-26 21:10:14 -0300

    Finish test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Header } from '../components/Header';
 import { MyTasksList } from '../components/MyTasksList';
 import { TodoInput } from '../components/TodoInput';
+import { Alert } from 'react-native';
 
 interface Task {
   id: number;
@@ -11,18 +12,32 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if(newTaskTitle){
+      const newTask = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      }
+      setTasks(oldTasks => [...oldTasks, newTask]);
+    } else {
+      Alert.alert('No title informed', 'Please, type some title to your task');
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const myUpdatedTasks = tasks.map((task) => {
+      task.id === id ? task.done = !task.done : task.done;
+      return task; 
+    });
+
+    setTasks(myUpdatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldTasks => oldTasks.filter(task => task.id !== id));
   }
 
   return (
